401f48d716d02b391d4b70a865055fbab38f787f
Godwin-durai <godwindurai6@gmail.com>
2023-06-26 10:41:57 +0530

    update

--- src/main/java/com/onesofts/customer/entity/Customer.java
+++ src/main/java/com/onesofts/customer/entity/Customer.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="customer")
+@Table(name="customerCall")
 public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
